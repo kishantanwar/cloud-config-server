0a3284c196012f986e2b709722b5bf58ff81dd50
kishantanwar <kishan.mca.du@gmail.com>
2021-07-31 00:14:37 +0530

    cloud-config-server

---
+++ src/test/java/com/epam/config/cloud/spring/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.config.cloud.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
